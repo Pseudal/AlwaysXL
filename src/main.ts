d02430c538b60f4d0d62a909fb24ec649c3446c3
Pseudal <adrien.g@gmx.fr>
2022-12-25 03:18:27 +0100

    up

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ const MOD_NAME = "alwaysxl";
 
 main();
 
+function SetXl() {
+
+}
+
 function main() {
   // Instantiate a new mod object, which grants the ability to add callback functions that
   // correspond to in-game events.
@@ -11,6 +15,7 @@ function main() {
 
   // Register a callback function that corresponds to when a new run is started.
   mod.AddCallback(ModCallback.POST_GAME_STARTED, postGameStarted);
+  mod.AddCallback(ModCallback.POST_CURSE_EVAL, SetXl);
 
   // Print a message to the "log.txt" file.
   Isaac.DebugString(`${MOD_NAME} initialized.`);
